b675fc1efc5906de5cd78f464af8a3c59d6c9404
Piyush <piyushranjan072@gmail.com>
2019-02-11 17:28:11 +0530

    updated timetable layout

--- home/urls.py
+++ home/urls.py
@@ -5,8 +5,4 @@ urlpatterns = [
     path('timetable/', views.timetable, name='timetable'),
     path('faculty/', views.faculty, name='faculty'),
     path('developers/', views.developers, name='developers'),
-    path('timetable/sem2', views.sem2, name='sem2'),
-    path('timetable/sem4', views.sem4, name='sem4'),
-    path('timetable/sem6', views.sem6, name='sem6'),
-    path('timetable/sem8', views.sem8, name='sem8'),
 ]
--- home/views.py
+++ home/views.py
@@ -9,17 +9,3 @@ def faculty(request):
 
 def developers(request):
 	return render_to_response('developers.html')
-
-def sem2(request):
-	return render_to_response('sem2.html')
-
-def sem4(request):
-	return render_to_response('sem4.html')
-
-
-def sem6(request):
-	return render_to_response('sem6.html')
-
-
-def sem8(request):
-	return render_to_response('sem8.html')
